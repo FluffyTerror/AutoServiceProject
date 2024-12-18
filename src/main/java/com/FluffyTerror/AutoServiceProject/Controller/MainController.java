491fdcfefb12f7a577ffc8075fd805cbd91ed5e4
FluffyTerror <8353207@gmail.com>
2024-12-18 21:49:58 +0300

    exit html

--- src/main/java/com/FluffyTerror/AutoServiceProject/Controller/MainController.java
+++ src/main/java/com/FluffyTerror/AutoServiceProject/Controller/MainController.java
@@ -4,10 +4,8 @@ import com.FluffyTerror.AutoServiceProject.Service.CarOwnerService;
 import com.FluffyTerror.AutoServiceProject.Service.RepairService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
@@ -27,8 +25,5 @@ public class MainController {
     public String exit(){
         return "exit";
     }
-
-
-
 
 }
